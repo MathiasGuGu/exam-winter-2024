d7972d1a78a61f22e8a35ee68d9ab04912b33a41
mathiasgugu <magugu@Mathiass-MacBook-Pro.local>
2023-12-18 18:24:33 +0100

    fixed credits bug

--- r/register/index.ts
+++ r/register/index.ts
@@ -49,7 +49,7 @@ form.addEventListener("submit", async (e) => {
     name: loginResults.name,
     email: loginResults.email,
     avatar: loginResults.avatar,
-    credit: loginResults.credits,
+    credits: loginResults.credits,
   };
 
   save("user", JSON.stringify(user));
